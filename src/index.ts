b89935e5a6cfef9321bbb731acbf2aabc3bb1f96
t.egawa <egawa.takeki@classmethod.jp>
2024-09-25 11:05:27 +0900

    localhostの記述を削除

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at ${port}`);
 });
