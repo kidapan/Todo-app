878cc939f499f92884ce6105f84373ea26812122
Kidapan <kida.maiko@classmethod.jp>
2024-10-30 15:33:35 +0900

    10-30 Constructorまで

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
+  const mytodo = new Todo("朝食べる", false)
   const { message } = req.query;
-  res.json({ message: message });
+  res.json({ todo: { content: mytodo.content, check: mytodo.check} });
 });
 
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
 });
+
+class Todo{
+  constructor(public readonly content: string , public readonly check: boolean){}
+}
